0a9b799fed5433accb27db2c4c8b42bb58c9f725
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2023-11-30 17:05:29 +0100

    first

---
+++ read.rb
@@ -0,0 +1,19 @@
+require_relative 'lib/cpee/transformation/bpmn2'
+require_relative 'lib/cpee/transformation/mermaid'
+require_relative 'lib/cpee/transformation/transformer'
+require_relative 'lib/cpee/transformation/cpee'
+
+mm = Dir.glob(File.join(__dir__,'data','**/*.mmd'))
+gv = Dir.glob(File.join(__dir__,'data','**/*.gv'))
+bpmn = Dir.glob(File.join(__dir__,'data','**/*.bpmn'))
+
+source = CPEE::Transformation::Source::BPMN2.new(File.read(bpmn[0]))
+pp source.graph
+
+# source = CPEE::Transformation::Source::Mermaid.new(File.read(mm[0]))
+# pp source.graph
+
+# trans = CPEE::Transformation::Transformer.new(source)
+# trans.build_traces
+# trans.build_tree(false)
+# puts trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
