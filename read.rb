3623ef07a485196cc3050ae04884fe88e1803b72
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2023-11-30 22:15:07 +0100

    progress

--- read.rb
+++ read.rb
@@ -8,11 +8,25 @@ mm = Dir.glob(File.join(__dir__,'data','**/*.mmd'))
 gv = Dir.glob(File.join(__dir__,'data','**/*.gv'))
 bpmn = Dir.glob(File.join(__dir__,'data','**/*.bpmn'))
 
-#source = CPEE::Transformation::Source::BPMN2.new(File.read(bpmn[0]))
-#pp source.graph
+### test BPMN2
+# source = CPEE::Transformation::Source::BPMN2.new(File.read(bpmn[0]))
+# pp source.graph
 
-source = CPEE::Transformation::Source::Mermaid.new(File.read('data/mermaid/1_2.mmd'))
-trans = CPEE::Transformation::Transformer.new(source)
-trans.build_traces
-trans.build_tree(false)
-puts trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
+### test Mermaid
+# source = CPEE::Transformation::Source::Mermaid.new(File.read(f))
+# trans = CPEE::Transformation::Transformer.new(source)
+# traces = trans.build_traces
+# puts traces.legend
+# puts traces.to_s
+
+mm.each do |f|
+  source = CPEE::Transformation::Source::Mermaid.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree(false)
+  model = trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
+  File.write(f.sub(/\.mmd$/,'.xml'),model)
+end
+
+
+
