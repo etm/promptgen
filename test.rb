4e69354c4c31709117902634e946ec173b5017f4
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2023-12-01 03:49:10 +0100

    progress

--- test.rb
+++ test.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/ruby
 require_relative 'lib/cpee/transformation/mermaid'
 require_relative 'lib/cpee/transformation/transformer'
+require_relative 'lib/cpee/transformation/cpee'
 
 source = CPEE::Transformation::Source::Mermaid.new(File.read('data/mermaid/1_2.mmd'))
 trans = CPEE::Transformation::Transformer.new(source)
 traces = trans.build_traces
-puts traces.legend
-puts traces.to_s
-trans.build_tree(true)
+puts traces.legend(false)
+puts traces.to_s(false)
+trans.build_tree(false)
+puts source.tree.to_s
+# pp source.tree
+model = trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
+
+#puts model
