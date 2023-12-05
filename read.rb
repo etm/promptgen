1645d2fffe1a78590a39d605be552c87e287373c
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2023-12-05 04:29:25 +0100

    text generation depth first

--- read.rb
+++ read.rb
@@ -4,22 +4,16 @@ require_relative 'lib/cpee/transformation/mermaid'
 require_relative 'lib/cpee/transformation/graphviz'
 require_relative 'lib/cpee/transformation/transformer'
 require_relative 'lib/cpee/transformation/cpee'
+require_relative 'lib/cpee/transformation/text-df-PO-reduced'
+require_relative 'lib/cpee/transformation/text-df-PO-extended'
 
 mm = Dir.glob(File.join(__dir__,'data','**/*.mmd'))
 gv = Dir.glob(File.join(__dir__,'data','**/*.gv'))
 bpmn = Dir.glob(File.join(__dir__,'data','**/*.bpmn'))
 
-### test BPMN2
-# source = CPEE::Transformation::Source::BPMN2.new(File.read(bpmn[0]))
-# pp source.graph
-
-### test Mermaid
-# source = CPEE::Transformation::Source::Mermaid.new(File.read('data/mermaid/1_2.mmd'))
-# trans = CPEE::Transformation::Transformer.new(source)
-# traces = trans.build_traces
-# puts traces.legend
-# puts traces.to_s
-
+#########################################################################################################
+### Write CPEE trees (CPEE XML models) - use load model in the cpee
+#########################################################################################################
 mm.each do |f|
   source = CPEE::Transformation::Source::Mermaid.new(File.read(f))
   trans = CPEE::Transformation::Transformer.new(source)
@@ -43,4 +37,60 @@ bpmn.each do |f|
   trans.build_tree
   model = trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
   File.write(f.sub(/\.bpmn$/,'.xml'),model)
+end
+
+#########################################################################################################
+### Write-Depth First, Pre-Order traversed texts with reduced context (better for small models)
+#########################################################################################################
+mm.each do |f|
+  source = CPEE::Transformation::Source::Mermaid.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_reduced)
+  File.write(f.sub(/\.mmd$/,'.df-po-reduced.txt'),text)
+end
+gv.each do |f|
+  source = CPEE::Transformation::Source::Graphviz.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_reduced)
+  File.write(f.sub(/\.gv$/,'.df-po-reduced.txt'),text)
+end
+bpmn.each do |f|
+  source = CPEE::Transformation::Source::BPMN2.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_reduced)
+  File.write(f.sub(/\.bpmn$/,'.df-po-reduced.txt'),text)
+end
+
+#########################################################################################################
+### Write-Depth First, Pre-Order traversed texts with extended context (better for large, nested models)
+#########################################################################################################
+mm.each do |f|
+  source = CPEE::Transformation::Source::Mermaid.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_extended)
+  File.write(f.sub(/\.mmd$/,'.df-po-extended.txt'),text)
+end
+gv.each do |f|
+  source = CPEE::Transformation::Source::Graphviz.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_extended)
+  File.write(f.sub(/\.gv$/,'.df-po-extended.txt'),text)
+end
+bpmn.each do |f|
+  source = CPEE::Transformation::Source::BPMN2.new(File.read(f))
+  trans = CPEE::Transformation::Transformer.new(source)
+  trans.build_traces
+  trans.build_tree
+  text = trans.generate_model(CPEE::Transformation::Target::Text_df_PO_extended)
+  File.write(f.sub(/\.bpmn$/,'.df-po-extended.txt'),text)
 end
--- test.rb
+++ test.rb
@@ -2,17 +2,18 @@
 require_relative 'lib/cpee/transformation/mermaid'
 require_relative 'lib/cpee/transformation/graphviz'
 require_relative 'lib/cpee/transformation/transformer'
-require_relative 'lib/cpee/transformation/cpee'
+require_relative 'lib/cpee/transformation/text-df-PO-reduced'
 
-source = CPEE::Transformation::Source::Graphviz.new(File.read('data/gv/1_2.gv'))
-puts source.graph.to_s
+source = CPEE::Transformation::Source::Mermaid.new(File.read('data/mermaid/1_2.mmd'))
+# puts source.graph.to_s
 trans = CPEE::Transformation::Transformer.new(source)
+
 traces = trans.build_traces
-puts traces.legend
-puts traces.to_s
+# puts traces.legend
+# puts traces.to_s
+
 trans.build_tree(false)
 puts source.tree.to_s
-# # pp source.tree
-# model = trans.generate_model(CPEE::Transformation::Target::CPEE).to_s
-#
-# #puts model
+puts
+
+puts trans.generate_model(CPEE::Transformation::Target::Text_df_PO_reduced)
